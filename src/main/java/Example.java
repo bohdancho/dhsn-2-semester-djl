92b145358886e7e98969b2fcc0682a0416c40747
Bohdan Chornokondratenko <bohdan@Bohdans-MacBook-Air.fritz.box>
2025-05-29 15:53:50 +0200

    working for 0

---
+++ src/main/java/Example.java
@@ -0,0 +1,73 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import ai.djl.Application;
+import ai.djl.Model;
+import ai.djl.basicdataset.cv.classification.Mnist;
+import ai.djl.basicmodelzoo.basic.Mlp;
+import ai.djl.modality.cv.Image;
+import ai.djl.modality.cv.ImageFactory;
+import ai.djl.ndarray.types.Shape;
+import ai.djl.nn.Activation;
+import ai.djl.nn.Blocks;
+import ai.djl.nn.SequentialBlock;
+import ai.djl.nn.core.Linear;
+import ai.djl.training.DefaultTrainingConfig;
+import ai.djl.training.EasyTrain;
+import ai.djl.training.Trainer;
+import ai.djl.training.evaluator.Accuracy;
+import ai.djl.training.listener.TrainingListener;
+import ai.djl.training.loss.Loss;
+import ai.djl.training.util.ProgressBar;
+import ai.djl.translate.TranslateException;
+
+
+public class Example {
+
+	public static void main(String[] args) throws IOException, TranslateException {
+        long inputSize = 28*28;
+        long outputSize = 10;
+ 
+        SequentialBlock block = new SequentialBlock();
+        block.add(Blocks.batchFlattenBlock(inputSize));
+        block.add(Linear.builder().setUnits(128).build());
+        block.add(Activation::relu);
+        block.add(Linear.builder().setUnits(64).build());
+        block.add(Activation::relu);
+        block.add(Linear.builder().setUnits(outputSize).build());
+ 
+        System.out.println("123");
+ 
+        int batchSize = 32;
+        Mnist mnist = Mnist.builder().setSampling(batchSize, true).build();
+        mnist.prepare(new ProgressBar());
+ 
+        Model model = Model.newInstance("mlp");
+        model.setBlock(new Mlp(28 * 28, 10, new int[] {128, 64}));
+ 
+        DefaultTrainingConfig config = new DefaultTrainingConfig(Loss.softmaxCrossEntropyLoss())
+		    //softmaxCrossEntropyLoss is a standard loss for classification problems
+		    .addEvaluator(new Accuracy()) // Use accuracy so we humans can understand how accurate the model is
+		    .addTrainingListeners(TrainingListener.Defaults.logging());
+ 
+		// Now that we have our training configuration, we should create a new trainer for our model
+		Trainer trainer = model.newTrainer(config);
+ 
+		trainer.initialize(new Shape(1, 28 * 28));
+ 
+		// Deep learning is typically trained in epochs where each epoch trains the model on each item in the dataset once.
+		int epoch = 100;
+ 
+		EasyTrain.fit(trainer, epoch, mnist, null);
+		Path modelDir = Paths.get("build/mlp");
+		Files.createDirectories(modelDir);
+ 
+		model.setProperty("Epoch", String.valueOf(epoch));
+ 
+		model.save(modelDir, "mlp");
+		System.out.println(model);
+ 
+	}
+}
---
+++ src/main/java/Usage.java
@@ -0,0 +1,64 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import ai.djl.MalformedModelException;
+import ai.djl.Model;
+import ai.djl.basicmodelzoo.basic.Mlp;
+import ai.djl.inference.Predictor;
+import ai.djl.modality.Classifications;
+import ai.djl.modality.cv.Image;
+import ai.djl.modality.cv.ImageFactory;
+import ai.djl.modality.cv.util.NDImageUtils;
+import ai.djl.ndarray.NDArray;
+import ai.djl.ndarray.NDList;
+import ai.djl.translate.Batchifier;
+import ai.djl.translate.TranslateException;
+import ai.djl.translate.Translator;
+import ai.djl.translate.TranslatorContext;
+
+public class Usage {
+
+	public static void main(String[] args) throws IOException, MalformedModelException, TranslateException {
+		Image img = ImageFactory.getInstance().fromUrl("https://b2q7zb733u.ufs.sh/f/O3YjbOo3Nsti5piDbr4yLv9tSU7ow8JKQ3sPDZCHr1pERhgW");
+		img.getWrappedImage();
+		
+		Path modelDir = Paths.get("build/mlp");
+		Model model = Model.newInstance("mlp");
+		model.setBlock(new Mlp(28 * 28, 10, new int[] {128, 64}));
+		model.load(modelDir);
+		
+		Translator<Image, Classifications> translator = new Translator<Image, Classifications>() {
+
+		    @Override
+		    public NDList processInput(TranslatorContext ctx, Image input) {
+		        // Convert Image to NDArray
+		        NDArray array = input.toNDArray(ctx.getNDManager(), Image.Flag.GRAYSCALE);
+		        return new NDList(NDImageUtils.toTensor(array));
+		    }
+
+		    @Override
+		    public Classifications processOutput(TranslatorContext ctx, NDList list) {
+		        // Create a Classifications with the output probabilities
+		        NDArray probabilities = list.singletonOrThrow().softmax(0);
+		        List<String> classNames = IntStream.range(0, 10).mapToObj(String::valueOf).collect(Collectors.toList());
+		        return new Classifications(classNames, probabilities);
+		    }
+
+		    @Override
+		    public Batchifier getBatchifier() {
+		        return Batchifier.STACK;
+		    }
+		};
+
+		Predictor<Image, Classifications> predictor = model.newPredictor(translator);
+		Classifications classifications = predictor.predict(img);
+
+		System.out.println(classifications);
+
+	}
+
+}
